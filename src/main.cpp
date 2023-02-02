3e0aeecefd0118393ada65f7eb0bae88299de815
ChSt15 <chrissteffen98@gmail.com>
2023-02-02 10:37:11 +0100

    Lib updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,49 @@
 #include <Arduino.h>
 
+#include "ExVectrCore/scheduler.hpp"
+#include "ExVectrArduinoPlatform.hpp"
+
+class Test: public VCTR::Task_Threading {
+public:
+
+    Test() : VCTR::Task_Threading("Test Task", VCTR::eTaskPriority_Realtime, 1*VCTR::SECONDS) {}
+
+    void thread() {
+
+        Serial.println(String("Time: ") + VCTR::NOWSeconds());
+
+    }
+
+};
+
+class Blinky: public VCTR::Task_Threading {
+public:
+
+    VCTR::Platform::HAL_PinGPIO pinLED;
+
+    Blinky() : VCTR::Task_Threading("Blinky Task", VCTR::eTaskPriority_Realtime, 0.3*VCTR::SECONDS) {}
+
+    void init() {
+
+        pinLED.init(LED_BUILTIN, VCTR::GPIO_IOMODE_t::IOMODE_OUTPUT);
+
+    }
+
+    void thread() {
+
+        pinLED.setPinValue(!pinLED.getPinValue());
+
+    }
+
+};
+
+Test t;
+Blinky b;
+
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    VCTR::Task_Threading::schedulerTick();
 }
