94bd7bb5ea71a5ceffdd380be30ef434741db491
ChSt15 <chrissteffen98@gmail.com>
2025-03-31 18:44:19 +0200

    Added UBLOX gnss library Implementation

---
+++ src/ublox_gnss.cpp
@@ -0,0 +1,168 @@
+#include "ublox_gnss.hpp"
+
+using namespace VCTR;
+
+void SNSR::UbloxSerialGNSS::_getData() {
+
+    auto time = Core::NOW();
+
+    numSats_ = gnss_.getSIV();
+
+    float positionDeviation = (float)gnss_.getHorizontalAccEst()/1000.0f;
+    float altitudeDeviation = (float)gnss_.getVerticalAccEst()/1000.0f;
+    float velocityError = (float)gnss_.getSpeedAccEst()/1000.0f;
+
+    SNSR::GNSSData gnssData;
+    gnssData.numSats = numSats_;
+    gnssData.position(0) = (double)gnss_.getLatitude()/1e7*DEG_TO_RAD;
+    gnssData.position(1) = (double)gnss_.getLongitude()/1e7*DEG_TO_RAD;
+    gnssData.position(2) = (double)gnss_.getAltitude()/1000.0;
+
+    gnssData.positionCov(0) = positionDeviation*positionDeviation;
+    gnssData.positionCov(1) = positionDeviation*positionDeviation;
+    gnssData.positionCov(2) = altitudeDeviation*altitudeDeviation;
+
+    gnssData.velocity(0) = (float)gnss_.getNedNorthVel()/1000.0;
+    gnssData.velocity(1) = -(float)gnss_.getNedEastVel()/1000.0;
+    gnssData.velocity(2) = -(float)gnss_.getNedDownVel()/1000.0;
+
+    gnssData.velocityCov = Math::Vector_F(velocityError*velocityError);
+
+    //LOG_MSG("NEW GNSS DATA: \n lat: %.10f, \n lon: %.10f, \n Alt: %.2f, \n Sats: %d, pdevh: %.2f, pdevv: %.2f, vdev: %.2f\n", gnssData.latitude, gnssData.longitude, gnssData.altitude, gnssData.numSats, positionDeviation, altitudeDeviation, velocityError);
+
+    //int64_t tow = gnss_.getTimeOfWeek()*MILLISECONDS;
+
+    gnssData.positionValid = gnssData.velocityValid = numSats_ >= 5;// && gnss_.getGnssFixOk();
+
+    gnssTopic_.publish(Core::Timestamped<SNSR::GNSSData>(gnssData, time));
+
+}
+
+
+bool SNSR::UbloxSerialGNSS::initSensor(HAL::DigitalIO &ioBus) {
+
+    taskInit();
+
+    return getInitialised();
+
+}
+
+
+void SNSR::UbloxSerialGNSS::taskThread() {
+
+    readGNSS();
+
+}
+
+
+bool SNSR::UbloxSerialGNSS::readGNSS() {
+
+    if (usbPassthrough_) {
+        
+        while (serialPort_->available()) Serial.write(serialPort_->read());
+        while (Serial.available()) serialPort_->write(Serial.read());
+        
+        return false;
+    }
+
+    //VRBS_MSG("GNSS Driver read update\n");
+
+    bool retVal = false;
+
+    if (gnss_.getPVT(0)) { //Read data but tell library to not waste time waiting.
+
+        _getData();
+        lastMeasurement_ = Core::NOW();
+
+        retVal = true;
+
+    } else if (Core::NOW() - lastMeasurement_ >= 500*Core::MILLISECONDS) {
+
+        //gnss_.factoryDefault(100);
+        //init();
+        //gnss_.flushPVT();
+        lastMeasurement_ = Core::NOW();
+
+        LOG_MSG("Long time past sice last measurment. Flushed PVT\n");
+
+    }
+
+    //LOG_MSG("LOOP\n");
+
+    return retVal;
+
+}
+
+
+
+void SNSR::UbloxSerialGNSS::setupSerial(uint32_t baudRate) {
+
+    #ifdef ESP32 
+
+    serialPort_->begin(baudRate, 134217756U, rxPin_, txPin_);
+
+    #else
+
+    serialPort_->begin(baudRate);
+
+    #endif
+
+}
+
+
+
+void SNSR::UbloxSerialGNSS::taskInit() {
+
+    if (usbPassthrough_) {
+        LOG_MSG("GNSS Driver setup for gnss serial passthrough!...\n");
+        serialPort_->begin(115200);
+        return;
+    }
+
+    VRBS_MSG("Starting up gnss module...\n");
+
+    setupSerial(115200);
+    if (!gnss_.begin(*serialPort_)) {
+
+        LOG_MSG("Failed to initialise GNSS module at 115200. Looks like its isnt setup yet. Attempting 9600...\n");
+
+        setupSerial(9600);
+        if (!gnss_.begin(*serialPort_)) {
+            LOG_MSG("Failed to initialise GNSS module at 9600. GNSS task will be paused and exit.\n");
+            setInitialised(false);
+            setPaused(true);
+            return;
+        }
+
+        LOG_MSG("GNSS was initialised at 9600. Will be reconfigured in next step.\n");
+
+    }
+
+    //Serial.begin(String("Staring GNSS module with ") + (moduleStatus_ == eModuleStatus_t::eModuleStatus_NotStarted ? "115200":"9600") + " baud...");
+
+    VRBS_MSG("Configuring GNSS module...\n");
+
+    gnss_.setSerialRate(115200);
+    delay(10);
+    setupSerial(115200);
+
+    gnss_.setUART1Output(COM_TYPE_UBX);
+    gnss_.setNavigationFrequency(10);
+    gnss_.setAutoPVT(true);
+    gnss_.setDynamicModel(DYN_MODEL_AIRBORNE2g);
+    gnss_.enableGNSS(true, sfe_ublox_gnss_ids_e::SFE_UBLOX_GNSS_ID_GPS);
+    gnss_.enableGNSS(true, sfe_ublox_gnss_ids_e::SFE_UBLOX_GNSS_ID_GALILEO);
+    gnss_.enableGNSS(true, sfe_ublox_gnss_ids_e::SFE_UBLOX_GNSS_ID_GLONASS);
+    gnss_.enableGNSS(false, sfe_ublox_gnss_ids_e::SFE_UBLOX_GNSS_ID_IMES);
+    gnss_.enableGNSS(false, sfe_ublox_gnss_ids_e::SFE_UBLOX_GNSS_ID_QZSS);
+    gnss_.enableGNSS(false, sfe_ublox_gnss_ids_e::SFE_UBLOX_GNSS_ID_SBAS);
+    gnss_.enableGNSS(false, sfe_ublox_gnss_ids_e::SFE_UBLOX_GNSS_ID_BEIDOU);
+
+    gnss_.saveConfiguration();
+
+    VRBS_MSG("GNSS Module configured.\n");
+
+    //setPriority(150);
+
+}
+
---
+++ src/ublox_gnss.hpp
@@ -0,0 +1,93 @@
+#ifndef EXVECTRSENSOR_UBLOXGNSS_H
+#define EXVECTRSENSOR_UBLOXGNSS_H
+
+#include "ExVectrCore/task_types.hpp"
+#include "ExVectrCore/scheduler2.hpp"
+
+#include "ExVectrHAL/digital_io.hpp"
+
+#include "ExVectrSensor/gnss.hpp"
+
+#include "Arduino.h"
+
+#include "gnssdriver/src/SparkFun_u-blox_GNSS_Arduino_Library.h"
+
+
+namespace VCTR
+{
+
+    namespace SNSR
+    {
+
+        
+        class UbloxSerialGNSS: public GNSS, public Core::Task_Periodic {
+        public:
+        
+            /**
+             * @param serialPort Pointer to serial port to use. If non default pins used then setup before init run.
+             * @param usbPassthrough If true then gps wont be setup and serial data will be passed to USB serial.
+             */
+            UbloxSerialGNSS(HardwareSerial& serialPort, int rxPin = -1, int txPin = -1) : Task_Periodic("Ublox GNSS", 2*Core::MILLISECONDS) {
+                serialPort_ = &serialPort;
+                rxPin_ = rxPin;
+                txPin_ = txPin;
+                usbPassthrough_ = false;// usbPassthrough;
+                Core::getSystemScheduler().addTask(*this);
+            }
+        
+            /**
+             * @brief Initialises and sets sensors settings.
+             * @param ioBus Which bus to use for communications.
+             * @return true if successfull and sensor is running, false otherwise.
+             */
+            bool initSensor(HAL::DigitalIO &ioBus);
+
+            /**
+             * @brief Makes the sensor read the values and publish them to the topic.
+             * @note Implemented by child class.
+             * @return true if reading was successfull. False otherwise.
+             */
+            bool readGNSS() override;
+
+            /**
+             * @brief Initialises sensor and expected to be called once at start by scheduler
+             */
+            void taskInit() override;
+
+            /**
+             * @brief main task thread that reads all sensor data and publishes it. To be called by scheduler.
+             */
+            void taskThread() override;
+        
+        
+        private:
+        
+            void setupSerial(uint32_t baudRate);
+        
+            void _getData();
+        
+            int rxPin_;
+            int txPin_;
+        
+            uint8_t numSats_ = 0;
+        
+            //Values used to determine true fix
+            uint8_t minNumSats_ = 5;
+            float maxError_ = 600;
+        
+            int64_t lastMeasurement_ = 0;
+        
+            HardwareSerial* serialPort_;
+            //uint32_t serialBaudMulti_ = 1;
+            bool usbPassthrough_ = false;
+        
+            SFE_UBLOX_GNSS gnss_;
+        
+            
+        };
+
+    }
+
+}
+
+#endif
