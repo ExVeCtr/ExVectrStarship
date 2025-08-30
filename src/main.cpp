f21679ac2cee47fe475f936236dfc9657fe94ea7
chst15 <chrissteffen98@gmail.com>
2025-08-30 21:05:29 +0200

    Created Bellyflop mission

--- src/main.cpp
+++ src/main.cpp
@@ -455,8 +455,13 @@ public:
         missionList_.append(&defaultMission_);     // Add the mission guidance task to the list of missions
         missionList_.append(&missionWaypointTask); // Add the mission waypoint task to the list of missions
 
-        missionWaypointTask.addWaypoint({0, 0, 0.3}, 100, 3, 1, 5, 0 * Core::SECONDS);                            // Add a waypoint to the mission waypoint task
+        missionWaypointTask.addWaypoint({0, 0, 0.3}, 0, 3, 1, 5, 0 * Core::SECONDS);                              // Add a waypoint to the mission waypoint task
         missionWaypointTask.addWaypoint({0, 0, 1}, 0.5, 3, 1, 0.5, 15 * Core::SECONDS, 10 * Core::SECONDS, true); // Add a waypoint to the mission waypoint task
+        missionWaypointTask.addWaypoint({0, 5, 2}, 1, 0.2, 0.2, 0.2, 0, 10 * Core::SECONDS);                      // Add a waypoint to the mission waypoint task
+        missionWaypointTask.addWaypoint({0, 5, 120}, 7, 3, 2, 3, 5 * Core::SECONDS);                              // Add a waypoint to the mission waypoint task
+        missionWaypointTask.setNextMission(&missionBellyflop);
+        missionBellyflop.setNextMission(&missionFreefallTask); // Set the next mission of the belly flop mission to the free fall mission
+        missionFreefallTask.setNextMission(&defaultMission_);  // Set the next mission of
     }
 
     void taskThread() override
